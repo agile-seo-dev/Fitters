7d6750c5b9a8800324d61228110619e5a53bb4c9
agile-seo-dev <agile-seo-dev@users.noreply.github.com>
2023-12-24 04:10:44 +0000

    Initial Commit 1

--- .scripts/setup.ps1
+++ .scripts/setup.ps1
@@ -6,7 +6,7 @@ Param(
   [String]$AzureTenantId,
   [ValidateLength(4, 17)]
   [String]$ProjectName,
-  [String]$AzureSqlLogin = "SqlAdmin"
+  [String]$AzureSqlLogin = "bone.r28@hotmail.com"
 )
 
 $scriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
@@ -39,7 +39,7 @@ if (-not $GitHubRepositoryName) {
 }
 
 if (-not $AzureLocation) {
-  $AzureLocation = "australiaeast"
+  $AzureLocation = "uksouth"
 }
 
 if (-not $AzureSubscriptionId) {
